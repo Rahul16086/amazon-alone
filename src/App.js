2d07fd75981da833603ddb456b9f087a8cbdb783
Rahul <rahul16086@gmail.com>
2021-07-07 15:49:14 +0530

    #1 Added routes and  nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path={"/login"}>
+            <h1>Login page</h1>
+          </Route>
+          <Route path={"/checkout"}>
+            <h1>Checkout page</h1>
+          </Route>
+          <Route path={"/"} exact>
+            <h1>Amazon clone Home page!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketOutlinedIcon from "@material-ui/icons/ShoppingBasketOutlined";
+
+const Header = () => {
+  return (
+    <nav className={"header"}>
+      <Link to={"/"}>
+        <img
+          className={"header__logo"}
+          src={"https://pngimg.com/uploads/amazon/amazon_PNG11.png"}
+          alt={"logo"}
+        />
+      </Link>
+
+      <div className={"header__search"}>
+        <input type={"text"} className={"header__searchInput"} />
+        <SearchIcon className={"header__searchIcon"} />
+      </div>
+
+      <div className={"header__nav"}>
+        <Link to={"/login"} className={"header__link"}>
+          <div className={"header__option"}>
+            <span className={"header__optionFirstLine"}>Hello Rahul!</span>
+            <span className={"header__optionSecondLine"}>Sign in</span>
+          </div>
+        </Link>
+        <Link to={"/"} className={"header__link"}>
+          <div className={"header__option"}>
+            <span className={"header__optionFirstLine"}>Returns</span>
+            <span className={"header__optionSecondLine"}>& Orders</span>
+          </div>
+        </Link>
+        <Link to={"/login"} className={"header__link"}>
+          <div className={"header__option"}>
+            <span className={"header__optionFirstLine"}>Your</span>
+            <span className={"header__optionSecondLine"}>Prime</span>
+          </div>
+        </Link>
+        <Link to={"/checkout"} className={"header__link"}>
+          <div className={"header__optionCart"}>
+            <ShoppingBasketOutlinedIcon />
+            <span className={"header__optionSecondLine header__cartCount"}>
+              0
+            </span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
